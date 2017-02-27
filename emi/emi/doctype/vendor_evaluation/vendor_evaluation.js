5c2dd74c05ee670345b597ecd341a38d3a0bfbc5
sagar <sagarshiragawakar@gmail.com>
2017-02-27 15:42:36 +0530

    Added New Doctype

---
+++ emi/emi/doctype/vendor_evaluation/vendor_evaluation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Indictranstech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Vendor Evaluation', {
+	refresh: function(frm) {
+
+	}
+});
