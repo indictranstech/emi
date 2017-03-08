6bdfde4ee8365b20b4eb3f3199ad347d8ab1b336
sagar <sagarshiragawakar@gmail.com>
2017-03-08 12:24:53 +0530

    add the print format changes ib job card & Report

--- emi/config/emi.py
+++ emi/config/emi.py
@@ -3,14 +3,105 @@ from frappe import _
 def get_data():
 	return [	
 	{
-		"label": _("Job Card"),
+		"label": _("Purchase"),
 		"icon": "icon-star",
 		"items": [
+			{
+				"type": "doctype",
+				"name": "Supplier",
+				"label": _("Supplier"),
+				"description": _("Supplier")
+			},
+			{
+				"type": "doctype",
+				"name": "Supplier Quotation",
+				"label": _("Supplier Quotation"),
+				"description": _("Supplier Quotation")
+			},
+			{
+				"type": "doctype",
+				"name": "Purchase Order",
+				"label": _("Purchase Order"),
+				"description": _("Purchase Order")
+			},
+			{
+				"type": "doctype",
+				"name": "Purchase Receipt",
+				"label": _("Purchase Receipt"),
+				"description": _("Purchase Receipt")
+			},
+		]
+	},
+	
+	{
+		"label": _("Sales"),
+		"icon": "icon-star",
+		"items": [
+			{
+				"type": "doctype",
+				"name": "Customer",
+				"label": _("Customer"),
+				"description": _("Customer")
+			},
+			{
+				"type": "doctype",
+				"name": "Quotation",
+				"label": _("Quotation"),
+				"description": _("Quotation")
+			},
+			{
+				"type": "doctype",
+				"name": "Sales Order",
+				"label": _("Sales Order"),
+				"description": _("Sales Order")
+			},
+			{
+				"type": "doctype",
+				"name": "Delivery Note",
+				"label": _("Delivery Note"),
+				"description": _("Delivery Note")
+			},
+		]
+	},
+	{
+		"label": _("Production"),
+		"icon": "icon-star",
+		"items": [
+			{
+				"type": "doctype",
+				"name": "BOM",
+				"label": _("BOM"),
+				"description": _("BOM")
+			},
+			{
+				"type": "doctype",
+				"name": "Production Order",
+				"label": _("Production Order"),
+				"description": _("Production Order")
+			},
 			{
 				"type": "doctype",
 				"name": "Job Card",
 				"label": _("Job Card"),
 				"description": _("Job Card")
+			},
+			{
+				"type": "doctype",
+				"name": "Production Planning Tool",
+				"label": _("Production Planning Tool"),
+				"description": _("Production Planning Tool")
+			},
+		]
+	},
+	{
+		"label": _("Stock"),
+		"icon": "icon-star",
+		"items": [
+			{
+				"type": "doctype",
+				"name": "Item",
+				"label": _("Item"),
+				"description": _("Item")
 			},
 		]
 	},
@@ -32,11 +123,6 @@ def get_data():
 				"type":"doctype",
 				"name": "Sub Category",
 				"description": _("Sub Category")
-			},
-			{
-				"type":"doctype",
-				"name": "Vendor Evaluation",
-				"description": _("Vendor Evaluation")
 			},
 			{
 				"type":"doctype",
@@ -63,13 +149,6 @@ def get_data():
 			"is_query_report": True,
 			"name": "Production%20Status",
 			"label": _("Production Status Report"),
-			"description": _("Production Report (Process Wise)"),
-			"doctype":"Job Card"
-		},
-		{
-			"type": "report",
-			"name": "Job Card Report",
-			"label": _("Job Card Report"),
 			"description": _("Production Report (Process Wise)"),
 			"doctype":"Job Card"
 		}
