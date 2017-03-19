8e1a10c723b1e9680928985b1ea33ebf7d0f3195
sagar <sagarshiragawakar@gmail.com>
2017-03-19 09:28:43 +0530

    added the Stock entry after the final inspection process

--- emi/emi/doctype/job_card/job_card.py
+++ emi/emi/doctype/job_card/job_card.py
@@ -7,7 +7,43 @@ import frappe
 from frappe.model.document import Document
 
 class JobCard(Document):
-	pass
+
+	def on_submit(self):
+		self.stock_entry_through_job_cart()
+
+	def stock_entry_through_job_cart(self):
+		si = None
+		for chld in self.job_order_detail:
+			if chld.process == "Final Inspection":
+				po = frappe.db.sql("""select * from `tabProduction Order` where name ='{0}'""".format(chld.production_order),as_dict=1)
+				print po, po[0]['fg_warehouse'], "uhhsdfdsb"
+				# po = frappe.db.sql("""select name, company, fg_warehouse, production_item,stock_uom  from `tabProduction Order` where name = {}""".format(chld.production_order))
+				 # = "PRO-00007"
+				si = frappe.get_doc({
+					"doctype": "Stock Entry",
+					"purpose": "Material Transfer",
+					"posting_date": chld.date,
+					"from_warehouse": po[0]['fg_warehouse'],
+					"to_warehouse": "Stores - E",
+					"items": get_order_items(po, chld, po[0]['fg_warehouse'], "Stores - E")
+				})
+		si.flags.ignore_mandatory = True
+		si.save(ignore_permissions=True)
+		si.submit()
+		frappe.db.commit()		
+
+
+def get_order_items(po, chld, s_warehouse, t_warehouse):
+	 	items = []
+	 	items.append({
+			"s_warehouse": s_warehouse,
+			"t_warehouse": t_warehouse,
+			"item_code": po[0]['production_item'],
+			"qty": float(chld.completed_job),
+			"uom": po[0]['stock_uom'],
+		})
+		return items		
+
 @frappe.whitelist()
 def get_info_production(doctype, txt, searchfield, start, page_len, filters):
 	return frappe.db.sql("""select name from `tabProduction Order` where status='In Process'""",as_list=1,debug=1)
