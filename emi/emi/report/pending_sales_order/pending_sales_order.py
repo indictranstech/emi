c577ae93abdcca7e1af4600f762a40cabf170e2a
sagar <sagarshiragawakar@gmail.com>
2017-08-08 18:35:55 +0530

    Print format changes & report fixes

--- emi/emi/report/pending_sales_order/pending_sales_order.py
+++ emi/emi/report/pending_sales_order/pending_sales_order.py
@@ -27,8 +27,9 @@ def get_data(filters):
 								order by so.name desc""".format(filters.name,filters.name),as_list=1)
 	else:
 		data=[]
-		total_row = [[u'Total', u'', u'', '','', u'', u'', u'',0.0, u'', u'', u'', u'',0.0,0.0,0.0,0.0, 0.0,0.0]]
-		last_row = [[u'', u'', u'', '','', u'', u'', u'',0.0, u'', u'', u'', u'',0.0,0.0,0.0,0.0, 0.0,0.0]]
+		
+		total_row = [['Total', '', '', '','', '', '', '',0.0, '', '', '', '',0.0,0.0,0.0,0.0, 0.0,0.0]]
+		last_row = [['', '', '', '','', '', '', '',0.0, '', '', '', '',0.0,0.0,0.0,0.0, 0.0,0.0]]
 		sales_orders = frappe.db.sql("select so.name from `tabSales Order` so where so.status = 'Draft' or so.status = 'To Deliver and Bill'""",as_dict=1)
 		for order in sales_orders:
 			data1 = []
@@ -67,10 +68,10 @@ def get_colums(filters):
 				   ("UOM") + ":Data:50",
 				   ("Qty") + ":Float:50" , 
 				   ("Delivered Qty") +":Float:50",
-				   ("Pending Qty") + ":Float:60",
-				   ("Rate") + ":Currency:120",
+				   ("Pending Qty") + ":Float:50",
+				   ("Rate") + ":Currency:50",
 				   ("Net Rate") + ":Currency:120", 
-		  		   ("Amount") + ":Currency:80",
+		  		   ("Amount") + ":Currency:50",
 		  		   ("Net Amount") + ":Currency:80" 
 			 ]
 				  	
@@ -86,34 +87,36 @@ def get_total_sales_amount(item_list):
 	net_amount =0.0
 	for item in item_list:
 		print "\nitem",item
-		amount = amount +flt(item[8])
-		qty = qty + flt(item[13])
-		pending_qty = pending_qty + flt(item[14])
-		rate = rate + flt(item[15])
-		net_rate = net_rate + flt(item[16])
-		amount1 =amount1 + flt(item[17])
-		net_amount = net_amount + flt (item[18])
-		
-	return [[u'Total', u'', u'', '','', u'', u'', u'',amount, u'', u'', u'', u'',qty,pending_qty,rate,net_rate, amount1, net_amount]]
-
+		amount = amount +float(item[8])
+		qty = qty + float(item[13])
+		pending_qty = pending_qty + float(item[14])
+		rate = rate + float(item[15])
+		net_rate = net_rate + float(item[16])
+		amount1 =amount1 + float(item[17])
+		net_amount = net_amount + float (item[18])
+	
+	return [['Total','', '', '','', '', '', '',amount, '', '', '', '',qty,pending_qty,rate,net_rate, amount1, net_amount]]
+	
 def get_last_total(last_row,item_list):
 	
-	amount = flt(last_row[0][8])
-	qty = flt(last_row[0][13])
-	pending_qty = flt(last_row[0][14])
-	rate = flt(last_row[0][15])
-	net_rate = flt(last_row[0][16])
-	amount1 = flt(last_row[0][17])
-	net_amount = flt(last_row[0][18])
+	amount = float(last_row[0][8])
+	qty = float(last_row[0][13])
+	pending_qty = float(last_row[0][14])
+	rate = float(last_row[0][15])
+	net_rate = float(last_row[0][16])
+	amount1 = float(last_row[0][17])
+	net_amount = float(last_row[0][18])
 
 
 	for item in item_list:
-		amount = amount +flt(item[8])
-		qty = qty + flt(item[13])
-		pending_qty = pending_qty + flt(item[14])
-		rate = rate + flt(item[15])
-		net_rate = net_rate + flt(item[16])
-		amount1 =amount1 + flt(item[17])
-		net_amount = net_amount + flt (item[18])
+		amount = amount +float(item[8])
+		qty = qty + float(item[13])
+		pending_qty = pending_qty + float(item[14])
+		rate = rate + float(item[15])
+		net_rate = net_rate + float(item[16])
+		amount1 =amount1 + float(item[17])
+		net_amount = net_amount + float (item[18])
+	
+	return [['Final Total', '', '', '','', '', '', '',amount,'', '', '', '',qty,pending_qty,rate,net_rate, amount1, net_amount]]
 		
-	return [[u'Final Total', u'', u'', '','', u'', u'', u'',amount, u'', u'', u'', u'',qty,pending_qty,rate,net_rate, amount1, net_amount]]
+	
