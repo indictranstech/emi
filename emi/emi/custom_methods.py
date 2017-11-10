2ec1c8ed84457b576a111447dd523dabccf7bc10
sagar <sagarshiragawakar@gmail.com>
2017-11-10 19:57:52 +0530

    [Fix Page Break]

--- emi/emi/custom_methods.py
+++ emi/emi/custom_methods.py
@@ -16,8 +16,9 @@ def calulate_consolidated_margin(doc, method):
 	# Calculat consolidate_margin = sum of item_price_rate - sum of total_margin
 	# Calculate price_list_total,and margin percentage
 	#Page-break
-	for row in self.items:
-		if len(self.items) > 8:
+	
+	for row in doc.items:
+		if len(doc.items) > 8:
 			if float(row.idx) % 8 == 0:
 				row.page_break = 1
 
