fae906c1947cc58570742fbd0fb21e14ff7ef4c9
prashante114 <prashante114@gmail.com>
2017-12-06 17:54:15 +0530

    solved:error save button in job card

--- emi/emi/doctype/job_card/job_card.js
+++ emi/emi/doctype/job_card/job_card.js
@@ -9,12 +9,15 @@ cur_frm.add_fetch('production_order', 'requested_for', 'requested_for');
 frappe.ui.form.on('Job Card', {
 	refresh: function(frm) {
 		
+		
 	},
 	validate: function(frm) {
 		var emp_mandatory_for = []
 		var supplier_mandotory = []
 		emp_processes = ["Pre Inspection","Punching","Bending", "Cutting", "Welding","Shearing","Cleaning","Final Inspection","Programming","Custom 1","Custom 2","Reduced Bending","Drilling","Tapping","Rolling","Assembly"]
 		supp_processes = ["Powder Coating", "Hot Dip Galvanizing", "Wet Coating"]
+		
+		
 		$.each(frm.doc.job_order_detail, function(idx, row) {
 			if (inList(emp_processes, row.process) && 
 				(emp_mandatory_for.indexOf(row.process) == -1) &&
@@ -27,17 +30,76 @@ frappe.ui.form.on('Job Card', {
 				supplier_mandotory.push(row.process)
 			}
 		})
+		// $.each(frm.doc.job_order_detail, function(idx, row) {
+		// 	if (inList(emp_processes, row.process) && (!row.employee_name)){
+		// 		employee_row.push(row.idx)
+		// 	}
+
+		// 	if (inList(supp_processes, row.process) && (!row.supplier)){
+		// 		supplier_row.push(row.idx)
+		// 	}
+		// })
+	
 		if (supplier_mandotory.length) {
-			frappe.throw("Supplier mandatory for processes: " + supplier_mandotory.join(","))		
+
+
+			frappe.msgprint("Supplier mandatory for processes: " + supplier_mandotory.join(","))
 		}
 		if(emp_mandatory_for.length) {
-			frappe.throw("Employee Name mandatory for processes: " + emp_mandatory_for.join(","))		
+
+			frappe.msgprint("Employee Name mandatory for processes: " + emp_mandatory_for.join(","))
 		}
+		
+		if(emp_mandatory_for.length == 0){
+			cur_frm.set_value("employee_name",1)
+		}
+		if(supplier_mandotory.length ==0){
+			cur_frm.set_value("supplier_name",1)
+		} 
+		
 	}
 });
 
 frappe.ui.form.on('Job Order Detail',{
 	/*To Check completed_job Qty is not greater than Assigned Qty*/
+	process:function(frm,cdt,cdn){
+		var d = locals[cdt][cdn]
+		emp_processes = ["Pre Inspection","Punching","Bending", "Cutting", "Welding","Shearing","Cleaning","Final Inspection","Programming","Custom 1","Custom 2","Reduced Bending","Drilling","Tapping","Rolling","Assembly"]
+		supp_processes = ["Powder Coating", "Hot Dip Galvanizing", "Wet Coating"]
+		
+		$.each(emp_processes, function(idx, value) {
+			if (d.process == value) {
+				console.log("\n\n\n\n****",d)
+				$("[data-fieldname=employee_name]").css({"border-color": "red"})
+		 	}
+		})
+		
+		// emp_processes = ["Pre Inspection","Punching","Bending", "Cutting", "Welding","Shearing","Cleaning","Final Inspection","Programming","Custom 1","Custom 2","Reduced Bending","Drilling","Tapping","Rolling","Assembly"]
+		// supp_processes = ["Powder Coating", "Hot Dip Galvanizing", "Wet Coating"]
+		
+		// $.each(supp_processes, function(idx, value) {
+		// 	if (d.process == value) {
+		// 		var field = frappe.utils.filter_dict(cur_frm.fields_dict["job_order_detail"].grid.grid_rows_by_docname[cdn].docfields,{"fieldname": "supplier"})[0];
+		// 		field.df.read_only = true;
+		// 		field.refresh();
+		// 	}
+		// })
+
+		// $.each(emp_processes, function(idx, value) {
+		// 	if (d.process == value) {
+				
+		// 		// var df = frappe.meta.get_docfield("Job Order Detail","supplier", frm.doc.name);
+		// 		// df.reqd = 0;
+		// 		var kf = frappe.meta.get_docfield("Job Order Detail","employee_name", frm.doc.name);
+		// 		kf.reqd = 1;
+		// 		console.log(d.process,value)
+
+		// 	}
+		// })
+		
+		
+	},
+
 	completed_job:function(frm, cdt, cdn){
 		var d = locals[cdt][cdn]
 		if(flt(d.job_allocated) < flt(d.completed_job) ){
@@ -95,5 +157,7 @@ cur_frm.fields_dict["production_order"].get_query = function(doc) {
 	}
 }
 cur_frm.add_fetch('employee_name', 'employee_name', 'employee')
+
+
 
 
