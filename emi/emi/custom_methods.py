36cd9155905ad21fce3538fb2a5522a1ab30d5d3
indictrans <prashant.j@indictranstech.com>
2018-07-20 19:32:09 +0530

    [Add][get_sales_invoices]

--- emi/emi/custom_methods.py
+++ emi/emi/custom_methods.py
@@ -279,4 +279,38 @@ def page_break(doc):
 					row.page_break = 1
 					page_break_idx += 15
 
+def get_sales_invoices():
+	#sales_incoice = frappe.get_all("Sales Invoice",filters = [['status', '!=', 'Cancelled']])
+	sales_incoice = frappe.db.sql(""" select name from `tabSales Invoice` where status != 'Cancelled' and name not in('SINV-00254','SINV-00296-1')""",as_dict=True)
+	for invoice in sales_incoice:
+		print "\n\n----------",invoice
+		doc = frappe.get_doc("Sales Invoice",invoice.get('name'))
+		if doc:
+			discount_amount = delivery_charge = other_amount =0.0
 	
+			if doc.shipping_rule:
+				shipping_rule_doc = frappe.get_doc("Shipping Rule",doc.shipping_rule)
+			''' Get Shipping Charges,VAT '''
+			
+			for tax in doc.taxes:
+				# if doc.shipping_rule:
+				# 	shipping_rule_doc = frappe.get_doc("Shipping Rule",doc.shipping_rule)
+				# 	if tax.account_head == shipping_rule_doc.account:
+				# 		doc.delivery_charge = tax.tax_amount
+				# 		tax.print_flag = 0
+
+				if tax.account_head == "Output VAT  - E":
+					doc.printformat_vat_tax = tax.tax_amount
+					tax.print_flag = 0
+
+				if doc.discount_amount:
+					discount_amount = doc.discount_amount
+
+			for tax in doc.taxes:
+				if tax.print_flag ==1:
+					other_amount = other_amount + tax.tax_amount
+			doc.printformat_other_total = other_amount
+			doc.printformat_net_total = (doc.total - doc.discount_amount) + flt(doc.printformat_other_total)
+			doc.save(ignore_permissions = True)
+			print "Done"
+
